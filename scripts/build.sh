449ef30b7ce75be116802f37a0627ab9e3dc70ed
ronsavage <ron@savage.net.au>
2015-09-12 11:54:34 +1000

    Initial version after splitting the FAQ out of the Marpa Papers

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+perl scripts/guide.pl
+
+DIR=Perl-modules/html/marpa.faq
+export DIR
+
+# $DR is doc root (on Debian's RAM disk): /dev/shm/html
+
+mkdir -p $DR/$DIR
+cp out/* $DR/$DIR
+
+mkdir -p ~/savage.net.au/$DIR
+cp out/* ~/savage.net.au/$DIR
+
+echo Copied HTML to ~/savage.net.au/... and $DR/$DIR
