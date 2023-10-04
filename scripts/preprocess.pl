d1a415b8124afdbd1443bef32bba51d63c2de7e1
Ron Savage <ron@savage.net.au>
2023-10-04 16:01:06 +1100

    Add version and date to FAQ. Update preproces.pl to identify questions

--- scripts/preprocess.pl
+++ scripts/preprocess.pl
@@ -10,14 +10,53 @@ my($in_file_name)	= 'guide/faq.txt';
 my($out_file_name)	= 'guide/faq.new';
 my(@lines)		= read_lines($in_file_name);
 
-say "$in_file_name. Lines: @{[$#lines + 1]}";
+say "Processing: $in_file_name. Line count: @{[$#lines + 1]}";
 
-for my $i (@lines)
+my(%messages, %offsets);
+my($link_number, $q_number, $text);
+
+$offsets{start_of_toc}	= 99999;
+$offsets{end_of_toc}	= 99999;
+
+for my $i (0 .. $#lines)
 {
+	# Find start and end of TOC.
+
+	if ($lines[$i] eq '##Table of Contents grouped by Topic')
+	{
+		$offsets{start_of_toc} = $i;
+
+		say "Start of TOC at line $i";
+	}
+	elsif ($lines[$i] eq '##Answers grouped by Topic')
+	{
+		$offsets{end_of_toc} = $i;
+
+		say "End of TOC @ at line $i";
+	}
 	
+	# Stockpile Questions and their ids.
+
+	if ( ($i >= $offsets{start_of_toc}) && ($i <= $offsets{end_of_toc}) )
+	{
+#		* [102 What is Libmarpa?](#q102)
+
+		if ($lines[$i] =~ /\[(\d+)\s+([^]]+)\]\(\#q(\d+)\)/)
+		{
+			$q_number	= $1;
+			$text		= $2;
+			$link_number	= $3;
+			say "Line: @{[$i + 1]} Q: <$q_number>. Link: <$link_number>. <$text>";
+
+			if ($q_number != $link_number)
+			{
+				say "Error. Mismatch between q number and link number";
+
+				exit;
+			}
+		}
+	}
 }
 
 write_text($out_file_name, join("\n", @lines) . "\n");
-
-#say "$in_file_name. Lines: @{[$#lines + 1]}";
 
