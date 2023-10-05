3cbf1abe30ae6dbeb312a2cafc395a30295f51c5
Ron Savage <ron@savage.net.au>
2023-10-05 20:14:24 +1100

    Convert code to use Getopt::Long and Pod::Usage, to support -v 0|1|2

--- scripts/preprocess.pl
+++ scripts/preprocess.pl
@@ -4,6 +4,10 @@ use 5.018;
 
 use File::Slurper qw/read_lines write_text/;
 
+use Getopt::Long;
+
+use Pod::Usage;
+
 use Syntax::Keyword::Match;
 
 # ------------------------------------------------
@@ -29,104 +33,174 @@ sub report_error
 
 # ------------------------------------------------
 
-my($in_file_name)	= 'guide/faq.txt';
-my($out_file_name)	= 'guide/faq.new';
-my(@lines)		= read_lines($in_file_name);
-
-say "Processing: $in_file_name. Line count: @{[$#lines + 1]}";
-
-my(%errors, %error_parameters, %link2question, %offsets, %question2link);
-my($link_number, $link_reference, $q_number, $text);
-
-# Link references:
-# <a name = 'q107'></a>
-# <a name = 'q155'></a>
-# Link targets:
-# * [102 What is Libmarpa?](#q102)
-# * [155 Where can I find a timeline (history) of parsing?](#q155)
-
-$errors{1}		= 'Duplicate question text';
-$errors{2}		= 'Duplicate link number';
-$errors{3}		= 'Mismatch between question number and link number';
-my($qr_link_reference)	= qr/a\s+name\s*=\s*'q(\d+)'><\/a>/;
-my($qr_link_target)	= qr/\[(\d+)\s+([^]]+)\]\(\#q(\d+)\)/;
-$offsets{start_of_toc}	= 99999;
-$offsets{end_of_toc}	= 99999;
-
-for my $i (0 .. $#lines)
+sub run
 {
-	# Find start and end of TOC.
+	my($option)		= @_;
+	my($in_file_name)	= 'guide/faq.txt';
+	my($out_file_name)	= 'guide/faq.new';
+	my(@lines)		= read_lines($in_file_name);
 
-	if ($lines[$i] eq '##Table of Contents grouped by Topic')
+	say "Processing: $in_file_name. Line count: @{[$#lines + 1]}" if ($$option{'verbose'});
+
+	my(%errors, %error_parameters, %link2question, %offsets, %question2link);
+	my($link_number, $link_reference, $q_number, $text);
+
+	# Link references:
+	# <a name = 'q107'></a>
+	# <a name = 'q155'></a>
+	# Link targets:
+	# * [102 What is Libmarpa?](#q102)
+	# * [155 Where can I find a timeline (history) of parsing?](#q155)
+
+	$errors{1}		= 'Duplicate question text';
+	$errors{2}		= 'Duplicate link number';
+	$errors{3}		= 'Mismatch between question number and link number';
+	my($qr_link_reference)	= qr/a\s+name\s*=\s*'q(\d+)'><\/a>/;
+	my($qr_link_target)	= qr/\[(\d+)\s+([^]]+)\]\(\#q(\d+)\)/;
+	$offsets{start_of_toc}	= 99999;
+	$offsets{end_of_toc}	= 99999;
+
+	for my $i (0 .. $#lines)
 	{
-		$offsets{start_of_toc} = $i;
+		# Find start and end of TOC.
 
-		#say "Start of TOC at line $i";
-	}
-	elsif ($lines[$i] eq '##Answers grouped by Topic')
-	{
-		$offsets{end_of_toc} = $i;
-
-		#say "End of TOC @ at line $i";
-	}
-	
-	# Stockpile Questions and their ids.
-
-	if ( ($i >= $offsets{start_of_toc}) && ($i <= $offsets{end_of_toc}) )
-	{
-		if ($lines[$i] =~ $qr_link_target)
+		if ($lines[$i] eq '##Table of Contents grouped by Topic')
 		{
-			$q_number	= $1;
-			$text		= $2;
-			$link_number	= $3;
+			$offsets{start_of_toc} = $i;
 
-			#say "Line: $i. Text: $lines[$i]";
+			say "Start of TOC at line $i" if ($$option{'verbose'} == 2);
+		}
+		elsif ($lines[$i] eq '##Answers grouped by Topic')
+		{
+			$offsets{end_of_toc} = $i;
 
-			# Stockpile info for the error reporter.
+			say "End of TOC @ at line $i" if ($$option{'verbose'} == 2);
+		}
+		
+		# Stockpile Questions and their ids.
 
-			$error_parameters{i}		= $i;
-			$error_parameters{link_number}	= $link_number;
-			$error_parameters{q_number}	= $q_number;
-			$error_parameters{text}		= $text;
-
-			# Validate that the question text is unique.
-
-			if ($question2link{$text})
+		if ( ($i >= $offsets{start_of_toc}) && ($i <= $offsets{end_of_toc}) )
+		{
+			if ($lines[$i] =~ $qr_link_target)
 			{
-				report_error(1, \%errors, \%error_parameters, \%link2question, \%question2link);
+				$q_number	= $1;
+				$text		= $2;
+				$link_number	= $3;
+
+				say "Line: $i. Text: $lines[$i]" if ($$option{'verbose'} == 2);
+
+				# Stockpile info for the error reporter.
+
+				$error_parameters{i}		= $i;
+				$error_parameters{link_number}	= $link_number;
+				$error_parameters{q_number}	= $q_number;
+				$error_parameters{text}		= $text;
+
+				# Validate that the question text is unique.
+
+				if ($question2link{$text})
+				{
+					report_error(1, \%errors, \%error_parameters, \%link2question, \%question2link);
+				}
+
+				# Validate that the link number is unique.
+
+				if ($link2question{$link_number})
+				{
+					report_error(2, \%errors, \%error_parameters, \%link2question, \%question2link);
+				}
+
+				# Validate that the question # matches the link number.
+
+				if ($q_number != $link_number)
+				{
+					report_error(3, \%errors, \%error_parameters, \%link2question, \%question2link);
+				}
+
+				$link2question{$link_number}	= $text;
+				$question2link{$text}		= $link_number;
+
 			}
+		}
+		elsif ($i > $offsets{end_of_toc})
+		{
+			say "Testing Line: $i. Text: $lines[$i]" if ($$option{'verbose'} == 2);
 
-			# Validate that the link number is unique.
-
-			if ($link2question{$link_number})
+			if ($lines[$i] =~ $qr_link_reference)
 			{
-				report_error(2, \%errors, \%error_parameters, \%link2question, \%question2link);
+				$link_reference = $1;
+
+				say "Line: $i. Text: $lines[$i]. Ref: <$link_reference>" if ($$option{'verbose'} == 2);
 			}
-
-			# Validate that the question # matches the link number.
-
-			if ($q_number != $link_number)
-			{
-				report_error(3, \%errors, \%error_parameters, \%link2question, \%question2link);
-			}
-
-			$link2question{$link_number}	= $text;
-			$question2link{$text}		= $link_number;
-
 		}
 	}
-	elsif ($i > $offsets{end_of_toc})
-	{
-		#say "Testing Line: $i. Text: $lines[$i]";
 
-		if ($lines[$i] =~ $qr_link_reference)
-		{
-			$link_reference = $1;
+	write_text($out_file_name, join("\n", @lines) . "\n");
 
-			#say "Line: $i. Text: $lines[$i]. Ref: <$link_reference>";
-		}
-	}
+} # End of run.
+
+# -----------------------------------------------
+
+my($option_parser) = Getopt::Long::Parser -> new();
+
+my(%option);
+
+if ($option_parser -> getoptions
+(
+ \%option,
+ 'help',
+ 'verbose=i',
+) )
+{
+	pod2usage(1) if ($option{'help'});
+
+	exit run(\%option);
+}
+else
+{
+	pod2usage(2);
 }
 
-write_text($out_file_name, join("\n", @lines) . "\n");
+__END__
+
+=pod
+
+=head1 NAME
+
+preprocess.pl - Convert guide/faq.txt into guide/faq.new
+
+=head1 SYNOPSIS
+
+preprocess.pl [options]
+
+	Options:
+	-help
+	-verbose 0|1
+
+All switches can be reduced to a single letter.
+
+Exit value: 0.
+
+=head1 OPTIONS
+
+=over 4
+
+=item -help
+
+Print help and exit.
+
+=item -verbose 0|1|2
+
+If used, print some progress reports.
+The bigger the number, the more reports printed.
+
+Defaults to 0: Do not print.
+
+=back
+
+=head1 DESCRIPTION
+
+Convert guide/faq.txt into guide/faq.new.
+
+=cut
 
